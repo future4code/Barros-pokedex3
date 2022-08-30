176f6d705315acdda5232891602ef8f9bda1a012
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-08-30 19:41:09 -0300

    Adicionar roots das cores

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -2,6 +2,14 @@ import styled, { createGlobalStyle } from "styled-components";
 
 
 export const GlobalStyle = createGlobalStyle `
+
+    :root {
+        --dark-blue: #0A285F;
+        --light-blue: #0075BE;
+        --dark-yellow: #D5A100;
+        --light-yellow: #FFCC00;
+        }
+
     * {
         margin: 0;
         padding: 0;
