5c647d8c9027bf0f6eaebd026f6c8eb11703aa36
Giovana Inez Vieira <gioivieira@gmail.com>
2022-09-06 15:00:34 -0300

    Ajustes de responsividade

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -23,6 +23,20 @@ export const Container = styled.main `
     p, li {
         font-size: 16px;
         line-height: 3vh;
+
+        @media screen and (max-width: 525px){
+            font-size: 3.5vw;
+            margin-bottom: 1vw;
+        }
+
+        @media screen and (min-width: 526px) and (max-width: 800px){
+            font-size: 2.5vw;
+            margin-bottom: 1vw;
+        }
+
+        @media screen and (min-width: 801px){
+            
+        }
     }
     ul {
         list-style: none;
@@ -30,10 +44,34 @@ export const Container = styled.main `
     h1 {
         font-size: 34px;
         line-height: 5vh;
+
+        @media screen and (max-width: 525px){
+            font-size: 5vw;
+        }
+
+        @media screen and (min-width: 526px) and (max-width: 800px){
+            font-size: 4vw;
+        }
+
+        @media screen and (min-width: 801px){
+            
+        }
     }
     h2 {
         font-size: 26px;
         line-height: 7vh;
+
+        @media screen and (max-width: 525px){
+            font-size: 5vw;
+        }
+
+        @media screen and (min-width: 526px) and (max-width: 800px){
+            font-size: 4vw;
+        }
+
+        @media screen and (min-width: 801px){
+            
+        }
     }
     button {
         cursor: pointer;
@@ -48,5 +86,32 @@ export const Container = styled.main `
         :hover {
             opacity: .8;
         }
+
+        @media screen and (max-width: 525px){
+            font-size: 2.5vw;
+            min-width: 15vw;
+            max-width: 20vw;
+            min-height: 2vh;
+            max-height: 5vh;
+            margin-top: 1vw;
+            padding: 0.5vw;
+        }
+
+        @media screen and (min-width: 526px) and (max-width: 800px){
+            font-size: 2vw;
+            min-width: 13vw;
+            max-width: 18vw;
+            min-height: 1vh;
+            max-height: 7vh;
+            margin-bottom: 1.5vw;
+            margin-top: 1vw;
+            padding: 0.6vw;
+        }
+
+        @media screen and (min-width: 801px){
+            
+        }
+
+
     }
 `
--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -32,4 +32,5 @@ export const HeaderSection = styled.header `
             }
         }
     }
+
 `
